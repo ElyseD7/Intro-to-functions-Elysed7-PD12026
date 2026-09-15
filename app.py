585c8783934f53e10a7f6706ee46f0c125630fef
ElyseD7 <elysed7@nycstudents.net>
2026-09-15 06:20:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,37 @@ t = Turtle()
 
 t.shape('turtle')
 
-t.forward(200) #inside parenthesees is argument
-turtle.done()
+#t.forward(200) #inside parenthesees is argument
+
+
+""" def message(input):
+    print(input)
+message("Hi turtle") """
+
+# t.right(90)
+# t.forward(200)
+# t.right(90)
+# t.forward(200)
+# t.right(90)
+# t.forward(200)
+
+# t.forward(150)
+# t.left(135)
+# t.forward(100)
+# t.left(90)
+# t.forward(105)
+
+# t.forward(80)
+# t.left(135)
+# t.forward(100)
+# t.left(135)
+# t.forward(80)
+# turtle.done()
+
+t.forward(100)
+t.left(90)
+t.forward(125)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(125)
