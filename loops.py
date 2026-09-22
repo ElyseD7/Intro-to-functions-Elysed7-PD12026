81d04234324787f33cee738baadc47c9f69d2fa5
ElyseD7 <elysed7@nycstudents.net>
2026-09-22 06:22:24 -0700

    sss

--- loops.py
+++ loops.py
@@ -44,10 +44,57 @@ t.shape('turtle')
 # triangle(100,120)
 # turtle.done()
 
-t.speed(100)
+t.speed(200)
 
-def square(x,y):
-    for i in range(60):
+def square(x):
+    for i in range(4):
         t.forward(x)
-        t.right(y+5)
-square(200,90)
+        t.right(90)
+#square(200)
+
+# for i in range(60):
+#     square(50)
+#     t.right(5)
+
+# def doublesquares(iRange):
+#     length = 25
+#     for i in range(iRange):
+#         square(length)
+#         length = length * 2
+# doublesquares(5) 
+# turtle.done()
+
+# def addsquare(iRange):
+#     length = 25
+#     for i in range(iRange):
+#         square(length)
+#         length += 5
+# addsquare(4) 
+
+# def specsquare(iRange):
+#     length = 25
+#     for i in range(iRange):
+#         square(length) 
+#         length += 5
+#         t.right(5)
+# specsquare(60)
+
+
+# for i in range(50):
+#     length = 25
+#     square(length)
+#     t.right(5)
+
+def star(x):
+    for i in range(5):
+        t.forward(x)
+        t.left(144)
+star(1)
+
+def specstar(iRange):
+    length = 25
+    for i in range(iRange):
+        star(length) 
+        length += 5
+        t.right(5)
+specstar(60)
